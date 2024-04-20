1281f30633590055d2c456ad4f3164fceafe3ec3
Stephanie Silva <124521846+ssilva24@users.noreply.github.com>
2024-04-19 20:45:19 -0400

    Add files via upload

---
+++ public/assets/views/main/resumeview.php
@@ -0,0 +1,16 @@
+<section id="resume">
+
+<br>
+    <h5> &nbsp  < Resume / > </h5>
+
+        <br>
+            &nbsp&nbsp
+            <img src="Resume.jpg" alt="my resume!" width = "500" height ="600" float= "left" style="border:3px solid #ad35e0"></img>
+
+                <br><br>
+                <h5> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 
+                    Thank you for visiting my homepage!</h5>
+                <br><br>
+
+
+</section>
