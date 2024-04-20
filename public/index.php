5fea7957781815c70aaf0cd6d17b4590be9c8dee
Stephanie Silva <124521846+ssilva24@users.noreply.github.com>
2024-04-19 20:37:46 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,16 +1,18 @@
 <?php
 
 require_once '../app/core/init.php';
+require_once '../app/core/Router.php';
 require_once '../app/core/routes.php';
-$env = parse_ini_file('../.env');
+require_once '../app/controllers/ContactController.php';
+
+//$env = parse_ini_file('../.env');
 require '../app/core/config.php';
 
 use app\core\Router;
 
 require_once __DIR__ . '/helpers.php';
 
-//to add a new route add to the app/core/routes.php array
 $router = new Router($routes);
 $router->serveRoute();
-?>
+
 
