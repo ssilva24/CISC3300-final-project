ae00e9733df70291d275cfc9b28a5760195a1219
Stephanie Silva <124521846+ssilva24@users.noreply.github.com>
2024-04-19 20:43:55 -0400

    Add files via upload

---
+++ public/assets/views/main/experienceview.php
@@ -0,0 +1,67 @@
+
+<section id="experience">
+<br><br>
+    <h5>  &nbsp < Experience / > </h5>
+            <ul>
+                <br>
+                    <h3>
+                         July 2021 - August 2021 </h3>
+
+                    <h6> 
+                    PENCIL, RXR REALTY Internship </h6>
+                    <br>
+
+                <h7>
+                
+                     Participated in a virtual internship and worked on a capstone project about affordable housing in New Rochelle.
+                    Submitted weekly deliverables and attended workshop and networking sessions.
+                     Once the Capstone Project was completed, presented it to the RXR Realty Company.</h7>
+                      <h8>   <br><br>
+                        
+                      Market Research  &nbsp Problem Solving   &nbsp Property Management   &nbsp Power Point
+                            </h8>
+                </ul>
+
+                <ul>
+                <br>
+                    <h3>
+                        June 2023 - August 2023 </h3>
+
+                    <h6>
+                    Fordham University Science Technology Entry Program (STEP) </h6>
+                    <br>
+
+                <h7>
+                     Collaborated with a team of 20 TCs to guide and lead over 150 
+                    students through advanced STEM classes.
+                    Supervised an Information Technology symposium; created and presented a workshop for students interested in technology.</h7>
+                    <h8>   <br><br>
+
+                      Leadership  &nbsp Teamwork   &nbsp Creativity   &nbsp Responsibility &nbsp Organization
+                            </h8>
+                </ul>
+
+                <ul>
+                <br>
+                    <h3>
+                        September 2023 - Present </h3>
+
+                    <h6> 
+                    Fordham University Office of Information Technology, IT Service Desk </h6>
+                    <br>
+
+                <h7>
+            
+                     Provide support to faculty and students seeking assistance 
+                     with technical issues for computer systems and software issues.
+                    Manage software installation/updating, antivirus installation, mobile  
+                    app configuration, hardware scans and running diagnostic programs
+                      to resolve problems as needed.</h7>
+
+                     <h8>   <br><br>
+                       
+                      Communication  &nbsp Troubleshooting   &nbsp Customer Service   &nbsp Technical &nbsp Detail Oriented
+                            </h8>
+                </ul>
+
+</section>
